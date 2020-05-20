f1c952f1e8bc21ed07de5c826ac9877070a9ce3c
Adam Conrad <conraada@flip2.engr.oregonstate.edu>
2020-05-19 17:28:34 -0700

    Added vocabulary

--- sanitize.py
+++ sanitize.py
@@ -2,26 +2,53 @@ import io
 import re
 
 def sanitize(filename):
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+    punctuations = '''!()[]{};:'"\,<>?@#$%^&*_~'''
     with open(filename, "r") as a_file:
         list = []
         for line in a_file:
+
+            #removes newlines
             line = line.strip()
+
+            #removes punctuation and concatenates
             no_punct = ""
             for char in line:
                if char not in punctuations:
                    no_punct = no_punct + char
             line = no_punct
+
+            #replaces punctuation with space
+            line = line.replace('-', ' ')
+            line = line.replace('/', ' ')
+            line = line.replace('.', ' ')
+
+            #removes value
             val = line[-1]
             line = line.replace(val, '')
-            line = re.sub('\s+',' ',line)
-            line = line.strip()
+
+            #converts to lowercase
+            if not line.islower():
+                line = line.lower()
+
+            # stores sentence with its value
             tup = (line, val)
             list.append(tup)
+
     return list
 
+def build_vocab(tuples):
+    words = ''
+    for sentence in tuples:
+        words += sentence[0] + ' '
+    unique_words = sorted(set(words.split()))
+    return unique_words
+
 def main():
-    tuples = sanitize('data/testSet.txt')
+    tuples = sanitize('data/trainingSet.txt')
+    vocab = build_vocab(tuples)
+    
+
 
 
 main()
